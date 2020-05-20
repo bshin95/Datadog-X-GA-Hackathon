52899291fe5628eee85fd76f2e679a389dbc3bcc
bshin95 <shinbryann@gmail.com>
2020-05-20 10:49:44 -0400

    finished weather api

--- frontend/src/Components/Weather/WeekContainer.jsx
+++ frontend/src/Components/Weather/WeekContainer.jsx
@@ -1,9 +1,13 @@
 import React from "react"
-// import apiConfig from "../apiKeys"
-
-// const apiConfig = `80bb87f8fbac9706c1f23b9ee85046c9`
+import DayCard from "./DayCard"
+// import apiConfig from "./apiKeys"
 
 class WeekContainer extends React.Component {
+  state = {
+    fullData: [],
+    dailyData: [],
+  }
+
   componentDidMount = () => {
     const weatherURL = `http://api.openweathermap.org/data/2.5/forecast?zip=11102&units=imperial&APPID=80bb87f8fbac9706c1f23b9ee85046c9`
 
@@ -23,20 +27,14 @@ class WeekContainer extends React.Component {
       })
   }
 
-  // formatDayCards = () => {
-  //   return this.state.dailyData.map((reading, index) => (
-  //     <DayCard reading={reading} key={index} />
-  //   ))
-  // }
+  formatDayCards = () => {
+    return this.state.dailyData.map((reading, index) => (
+      <DayCard reading={reading} key={index} />
+    ))
+  }
 
   render() {
-    const weatherURL = `http://api.openweathermap.org/data/2.5/forecast?zip=11102&units=imperial&APPID=80bb87f8fbac9706c1f23b9ee85046c9`
-
-    return (
-      <div>
-        <h1>Hello World!</h1>
-      </div>
-    )
+    return <div>{this.formatDayCards()}</div>
   }
 }
 
