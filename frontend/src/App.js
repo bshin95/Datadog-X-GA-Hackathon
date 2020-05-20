e6be76559e4c2f5106f952c9f2eec7b85cb2505b
bshin95 <shinbryann@gmail.com>
2020-05-19 20:29:13 -0400

    weather

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react"
 import "./App.css"
+import WeekContainer from "./components/Weather/WeekContainer"
 
 function App() {
-  return <div className="App">Social Network</div>
+  return (
+    <div className="App">
+      <div>Social Network</div>
+      <WeekContainer />
+    </div>
+  )
 }
 
 export default App
