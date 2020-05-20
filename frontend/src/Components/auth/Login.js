2d30b690c83264cca5b3f642bb31cbb90cc8a888
ReginaClarke <58581262+ReginaClarke@users.noreply.github.com>
2020-05-20 16:52:03 -0400

    package json was updated now have redux and other packages

--- frontend/src/Components/auth/Login.js
+++ frontend/src/Components/auth/Login.js
@@ -1,11 +1,52 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-export const Login = () => {
+  const { email, password } = formData;
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log("SUCCESS");
+  };
+
   return (
-    <div>
-      Login
-    </div>
-  )
-}
+    <>
+      <h1>Login</h1>
+      <form onSubmit={(e) => onSubmit(e)}>
+        <div>
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            value={email}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            value={password}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <input type="submit" value="Login" />
+      </form>
+      <p>
+        Don't have an account? <Link to="/register">Sign Up </Link>
+      </p>
+    </>
+  );
+};
 
-export default Login
+export default Login;
--- frontend/src/Components/auth/Register.js
+++ frontend/src/Components/auth/Register.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from 'axios'
+import { Link } from "react-router-dom";
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -18,26 +18,7 @@ const Register = () => {
     if (password !== password2) {
       console.log("Passwords do not match");
     } else {
-      const newUser = {
-        name,
-        email,
-        password
-      }
-
-      try {
-        const config = {
-          headers: {
-            'Content-Type': "application/json"
-          }
-        }
-
-        const body = JSON.stringify(newUser);
-
-        const res = await axios.post('/api/users', body, config)
-        console.log(res.data)
-      } catch (err) {
-        console.error(err.response.data)
-      }
+      console.log("success");
     }
   };
 
@@ -95,7 +76,7 @@ const Register = () => {
         <input type="submit" value="Register" />
       </form>
       <p>
-        Already have an account? <a href="login.html">Login</a>
+        Already have an account? <Link to="/login">Login</Link>
       </p>
     </>
   );
