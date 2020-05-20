81cd09ab05a4d02eca0ae271f24944680e184aeb
ReginaClarke <58581262+ReginaClarke@users.noreply.github.com>
2020-05-20 13:12:40 -0400

    minor clean up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,11 @@ import React from "react";
 import "./App.css";
 
 // import WeekContainer from "./components/Weather/WeekContainer"
-import Container from "./Components/Container";
+import Container from "./components/Container";
 
 function App() {
   return (
     <div className='App'>
-      <div>Social Network</div>
-      {/* <WeekContainer /> */}
       <Container />
     </div>
   );
