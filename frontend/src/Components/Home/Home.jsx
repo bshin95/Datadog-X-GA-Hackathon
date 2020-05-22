5989d415a4ec801d91ae8cd96d9639abe7fd982a
ReginaClarke <58581262+ReginaClarke@users.noreply.github.com>
2020-05-22 00:11:17 -0400

    tiles and routes done

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -9,7 +9,7 @@ class Home extends React.Component {
   render() {
     return (
       <>
-        <Search />
+        {/* <Search /> */}
 
         {/* <StoriesContainer /> */}
 
@@ -21,15 +21,63 @@ class Home extends React.Component {
                 alt="company logo"
                 className="hometiles"
               ></img>
-              <p>PawPrint</p>
+              <p>PAWPRINT</p>
             </div>
-            <Link to="/news">
-              <div>news</div>
-            </Link>
-            <div>2</div>
-            <div>Column</div>
-            <div>Column</div>
-            <div>Column</div>
+
+            <div>
+              <Link to="/news">
+                <img
+                  src="companynews.png"
+                  alt="company news"
+                  className="hometiles"
+                ></img>
+              </Link>
+              <p>NEWS</p>
+            </div>
+
+            <div>
+              <Link to="/newhires">
+                <img
+                  src="new-hires-puppies.png"
+                  alt="new hires"
+                  className="hometiles"
+                ></img>
+              </Link>
+              <p>NEW HIRES</p>
+            </div>
+
+            <div>
+              <Link to="/humanresources">
+                <img
+                  src="HRNews.png"
+                  alt="human resources"
+                  className="hometiles"
+                ></img>
+              </Link>
+              <p>HUMAN RESOURCES</p>
+            </div>
+
+            <div>
+              <Link to="/development">
+                <img
+                  src="tech.png"
+                  alt="development"
+                  className="hometiles"
+                ></img>
+              </Link>
+              <p>DEVELOPMENT</p>
+            </div>
+
+            <div>
+              <Link to="/eventcalendar">
+                <img
+                  src="announcements.png"
+                  alt="announcements"
+                  className="hometiles"
+                ></img>
+              </Link>
+              <p>EVENT CALENDAR</p>
+            </div>
           </div>
         </div>
       </>
